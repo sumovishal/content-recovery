c035c4b9e4a3306ee7a65eb1b61f00ce6fb0f673
Elad Michaeli <emichaeli@sumologic.com>
2020-11-12 20:21:08 -0800

    Address bug where API fails on missing optional field

--- src/dashboard.py
+++ src/dashboard.py
@@ -15,6 +15,7 @@ class Dashboard:
         self.description = row[1]
         self.title = row[2]
         self.timeRange = row[3]
+        self.refreshInterval = row[3]
         self.panels = []
         self.variables = []
 
@@ -24,7 +25,8 @@ class Dashboard:
             'name': self.name,
             'description': self.description,
             'title': self.title,
-            'timeRange': timerange.convertDbToApiTimeRange(self.timeRange),
+            'timeRange': None if (self.timeRange is None) else timerange.convertDbToApiTimeRange(self.timeRange),
+            'refreshInterval': self.refreshInterval,
             'panels': [panel.asdict() for panel in self.panels],
             'variables': [variable.asdict() for variable in self.variables]
         }
@@ -33,9 +35,9 @@ class Dashboard:
 class Panel:
     def __init__(self, row):
         self.key = row[0]
-        self.title = row[1]
+        self.title = row[1].decode("utf-8")
         self.panelType = row[2]
-        self.visualSettings = row[3]
+        self.visualSettings = row[3].decode("utf-8")
         self.timeRange = row[4]
         self.queries = []
         self.panels = []
@@ -48,7 +50,7 @@ class Panel:
             'panelType': self.panelType,
             'visualSettings': self.visualSettings,
             'timeRange': None if (self.timeRange is None) else timerange.convertDbToApiTimeRange(self.timeRange),
-            'queries': self.queries,
+            'queries': [query.asdict() for query in self.queries],
             'panels': self.panels,
             'variables': self.variables,
         }
@@ -111,7 +113,7 @@ def getVariableSourceDefinition(cqDbCursor, variableHexId, indent):
     csvValues = sourceDefinition[1]
     return {
         "variableSourceType": "CsvVariableSourceDefinition",
-        "values": csvValues
+        "values": csvValues.decode("utf-8")
     }
 
 
@@ -127,10 +129,9 @@ def getVariables(cqDbCursor, dashboardHexId, indent):
 
     variables = []
     for row in results:
-        print("{0}Creating query for panel with id: {1}".format(" "*indent, dashboardHexId))
+        logger.info("{0}Creating query for panel with id: {1}".format(" "*indent, dashboardHexId))
         variable = Variable(row[1])
         variable.sourceDefinition = getVariableSourceDefinition(cqDbCursor, row[0], indent)
-        print("{0}Variable created: {1}".format(" "*indent, variable.asdict()))
         variables.append(variable)
     return variables
 
@@ -147,9 +148,8 @@ def getQueries(cqDbCursor, panelHexId, indent):
 
     queries = []
     for row in results:
-        print("{0}Creating query for panel with id: {1}".format(" "*indent, panelHexId))
+        logger.info("{0}Creating query for panel with id: {1}".format(" "*indent, panelHexId))
         query = Query(row)
-        print("{0}Query created: {1}".format(" "*indent, query.asdict()))
         queries.append(query)
     return queries
 
@@ -169,13 +169,12 @@ def getPanels(cqDbCursor, dashboardHexId, indent):
         if row[2] != "SumoSearchPanel":
             print("{0}Panel type not supported: {1}", " "*indent, row[2])
             continue
-        print("{0}Creating panel for dashboard with id: {1}".format(" "*indent, dashboardHexId))
+        logger.info("{0}Creating panel for dashboard with id: {1}".format(" "*indent, dashboardHexId))
         panel = Panel(row)
         panel.queries = getQueries(cqDbCursor, row[5], indent)
         # All Salesforce's panels do not have parent panels / variables
         panel.panels = []
         panel.variables = []
-        print("{0}Panel created: {1}".format(" "*indent, panel.asdict()))
         panels.append(panel)
     return panels
 
@@ -184,8 +183,7 @@ def getPanels(cqDbCursor, dashboardHexId, indent):
 # it to create it as well via the API
 def createDashboard(cqDbCursor, name, description, targetExternalId, indent):
     dashboardHexId = convertToHex(targetExternalId)
-    print(dashboardHexId)
-    query = ("select title, time_range "
+    query = ("select title, time_range, refresh_interval "
              "from dashboard "
              "where hex_id='{0}'")
     cqDbCursor.execute(query.format(dashboardHexId))
@@ -194,9 +192,9 @@ def createDashboard(cqDbCursor, name, description, targetExternalId, indent):
         print("no dashboard with id: {0}".format(dashboardHexId))
         return
 
-    print("Creating dashboard: %s", name)
+    logger.info("Creating dashboard: {0}".format(name))
     newDashboard = Dashboard((name, description) + results[0])
     newDashboard.panels = getPanels(cqDbCursor, dashboardHexId, indent)
     newDashboard.variables = getVariables(cqDbCursor, dashboardHexId, indent)
 
-    print("dashboard json: {0}".format(json.dumps(newDashboard.asdict())))
+    print("Dashboard json: {0}".format(json.dumps(newDashboard.asdict())))
